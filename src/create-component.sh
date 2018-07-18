a1a93a715c652e6a1af9bfd1307b70ca1a429b58
Temisuki <temisuki@gmail.com>
2018-07-18 22:34:09 +0200

    Component creator

---
+++ src/create-component.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+cd app
+ls
+cp -r boilerplate/ "${1}/"
+cd "${1}"
+CLASS="$(echo ${1} | sed -r 's/(^|-)([a-z])/\U\2/g')"
+echo "${CLASS}"
+mv boilerplate.component.html "${1}.component.html"
+mv boilerplate.component.scss "${1}.component.scss"
+mv boilerplate.component.ts "${1}.component.ts"
+sed -i -e 's/boilerplate/new-component/g' "${1}.component.ts"
+sed -i -e "s/Boilerplate/${CLASS}/g" "${1}.component.ts"
